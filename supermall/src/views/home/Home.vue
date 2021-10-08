818d24c16a8e1afd2bbfd008eff254eea63872c7
1099050782 <1099050782@qq.com>
2021-10-08 13:27:25 +0800

    hj

--- supermall/src/views/home/Home.vue
+++ supermall/src/views/home/Home.vue
@@ -1,30 +1,36 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/common/navbar/NavBar";
 import HomeSwiper from "@/views/home/childComps/HomeSwiper";
+import RecommendView from "@/views/home/childComps/RecommendView";
+
 import {getHomeMultidata} from "@/network/home";
 
 export default {
   name: "Home",
   components: {
     NavBar,
-    HomeSwiper
+    HomeSwiper,
+    RecommendView
   },
   data() {
     return {
-      banners:[]
+      banners:[],
+      recommends: []
     }
   },
   created() {
     //1.请求多个数据
     getHomeMultidata().then(res => {
-      this.banners = res.data.banners.list;
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
     })
   }
 }
