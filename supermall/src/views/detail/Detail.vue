53a73994ebf4767eac62bc429e4fa7f94f4d0f8a
1099050782 <1099050782@qq.com>
2021-10-19 13:28:38 +0800

    hj

--- supermall/src/views/detail/Detail.vue
+++ supermall/src/views/detail/Detail.vue
@@ -2,24 +2,30 @@
   <div id="detail">
     <detail-nav-bar/>
     <detail-swiper :top-images="topImages"/>
+    <detail-base-info :goods="goods"/>
   </div>
 </template>
 
 <script>
 import DetailNavBar from "@/views/detail/childComps/DetailNavBar";
 import DetailSwiper from "@/views/detail/childComps/DetailSwiper";
-import {getDetail} from "@/network/detail";
+import DetailBaseInfo from "@/views/detail/childComps/DetailBaseInfo";
+
+import {getDetail, Goods} from "@/network/detail";
 
 export default {
   name: "Detail",
   components: {
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo,
+
   },
   data() {
     return {
       iid: null,
-      topImages: []
+      topImages: [],
+      goods: {}
     }
   },
   created() {
@@ -28,7 +34,12 @@ export default {
     //2.根据iid请求详情数据
     getDetail(this.iid).then(res => {
       //1.获取顶部的图片轮播数据
-      this.topImages = res.result.itemInfo.topImages
+      const data = res.result;
+      this.topImages = data.itemInfo.topImages
+      // 2.获取商品信息
+      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+
+
     })
   }
 }
